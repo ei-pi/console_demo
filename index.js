a3b7b8e92ef5a6f6966eeb617a7029e45d047ddc
ei-pi <91853103+ei-pi@users.noreply.github.com>
2023-06-08 19:19:48 -0400

    github action stuff

--- index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-const game = Game.getInstance(), player = game.player, Console = game.console;
-game.generateRandomObjects();
-player.inventory.appendItem(Guns["AKM"]);
-player.inventory.appendItem(Guns["M37"]);
-player.inventory.appendItem(Guns["AWP"]);
-player.inventory.appendItem(Guns["DEagle"]);
-player.inventory.appendItem(Guns["M16A4"]);
-//# sourceMappingURL=index.js.map
